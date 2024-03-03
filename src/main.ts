c0c864437d414c38312f0fa697ffc8965099f576
Roman Fokin <romanfokin1337@gmail.com>
2024-03-03 13:34:50 +0200

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = app.get(ConfigService).get('port');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port, () => {
     console.log(`listen at port ${port}`);
