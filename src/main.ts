3180909e07d83abffe6236362b9d0b41a9ee5bb6
Roman Fokin <romanfokin1337@gmail.com>
2024-03-03 13:22:31 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const port = app.get(ConfigService).get('port');
 
   const config = new DocumentBuilder()
